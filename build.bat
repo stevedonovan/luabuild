d55b86c8231079506a25c226916031b96361b65f
steve donovan <steve.j.donovan@gmail.com>
2012-06-05 15:27:39 +0200

    Windows build: batch files remain an interesting exercise in masochism

--- build.bat
+++ build.bat
@@ -1,21 +1,23 @@
 @echo off
 rem do we already have lake?
-for %%X in (lake.exe,lake.bat) do (
+for %%X in (lake.exe) do (
    set LAKE=%%~$PATH:X
    if defined LAKE goto :lake
 )
 :outf
-for %%X in (lua.exe,lua51.exe) do (
-  set LUA=%%~$PATH:X
-  if defined LUA goto :outl
+for %%X in (lua.exe,lua51.exe,luajit.exe) do (
+  set LUA="%%~$PATH:X"
+  if defined LUA goto :lua
 )
-:outl	
-if defined LUA ( 
-     set LAKE="%LUA%" lake
-) else (
-     echo no lua available...get lake.exe!!
-     exit /b
+echo no lua available...get lake.exe!!
+exit /b
+:lua
+%LUA% -llfs -e "print 'OK!'"  2>&1 | find "OK!" > nul
+if %errorlevel% neq 0  (
+    echo luafilesystem not installed for %LUA%
+    exit /b
 )
+set LAKE=%LUA% lake
 goto :go
 :lake
 rem if it's a batch needs special treatment
@@ -24,7 +26,7 @@ set LAKE="%LAKE%"
 if %errorlevel% equ 0 set LAKE=call %LAKE%
 :go
 rem and go!
-%LAKE%
+%LAKE% %*
 rem can now install the rest
 if %errorlevel% equ 0 (
   echo installing...
